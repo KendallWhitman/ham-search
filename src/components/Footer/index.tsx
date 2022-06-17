ec7f1798ad0a4ce8ded149ae0e72d07fc3bb0007
Kendall Whitman <kendallw@protonmail.com>
2022-06-16 20:27:45 -0500

    master: add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className='footer'>
+    <p className='footer__text'>Powered by <a href="https://hamdb.org" className='footer__link'>HamDB.org</a></p>
+  </footer>
+)
+
+export default Footer
