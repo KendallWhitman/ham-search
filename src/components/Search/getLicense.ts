aca5cb59de77ac8322eb869d2380106184fbea67
Kendall Whitman <kendallw@protonmail.com>
2022-06-16 21:24:36 -0500

    master: remove headers

--- src/components/Search/getLicense.ts
+++ src/components/Search/getLicense.ts
@@ -1,13 +1,5 @@
-const isProd = process.env.NODE_ENV !== 'development';
-
 const getLicense = async (value: string = '') => {
-  const headers = isProd ? {
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  } : {};
-
-  const response = await fetch(`https://api.hamdb.org/${value}/json/hamsearch`, headers);
+  const response = await fetch(`https://api.hamdb.org/${value}/json/hamsearch`);
   const { hamdb } = await response.json();
 
   return hamdb;
