55dfdaac75fbbc9bc8e269cb05174468c35cb30d
Kendall Whitman <kendallw@protonmail.com>
2022-06-21 11:44:48 -0500

    master: fix blank input while loading

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,12 +1,16 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { LicenseContext } from 'context';
 import getLicense from './getLicense';
 
 const Search = () => {
   const { licenseData, setLicenseData } = useContext(LicenseContext);
+  const [localValue, setLocalValue] = useState('');
   const recent = JSON.parse(localStorage.getItem('recent') || '[]');
 
-  const handleChange = (value: string) => setLicenseData({...licenseData, value: value});
+  const handleChange = (value: string) => {
+    setLocalValue(value);
+    setLicenseData({...licenseData, value: value});
+  }
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -26,7 +30,7 @@ const Search = () => {
           onChange={(e) => handleChange(e.target.value)}
           placeholder="Search by callsign"
           type='text'
-          value={licenseData.value || ""}
+          value={licenseData.value || localValue}
         />
         <button className='search__button'>Search</button>
       </form>
